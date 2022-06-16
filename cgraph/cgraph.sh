e0981dc69dde98736ea5526ab5d38307c295b5cd
ljs-2002 <ljs_2002@163.com>
2022-06-16 12:06:02 +0800

    修复了无法设置输入文件的bug并增加了帮助信息

--- cgraph/cgraph.sh
+++ cgraph/cgraph.sh
@@ -3,7 +3,7 @@
 function usage
 {
     echo "$*"
-    echo "usage: cgraph [-a <NUM>] [-d <NUM>] [-e <NUM>] [-r <NUM>] [-f <file1 file2...>] [-T <TYPE>] [-o <NAME>] [-h]"
+    echo "usage: cgraph [-a <NUM>] [-d <NUM>] [-e <NUM>] [-r <NUM>] [-f <file1 file2...>] [-T <TYPE>] [-o <NAME>] [-m <MODE>] [-h]"
     echo ""
     echo "-a [NUM] :"
     echo "  (*)0     Produce graphs only for main function"
@@ -17,9 +17,10 @@ function usage
     echo "-r [NUM] :"
     echo "     0     Disable output in the order in which the functions appear"
     echo "  (*)1     Enable output in the order in which the functions appear"
-    echo "-f (filename1 filename 2...) :"
+    echo "-f [filename] :"
     echo "           The files you want to produce call graph"
     echo "           (*) all .c file in the folder"
+    echo "           use \"filename1 filename2\" to set multiple files as input files" 
     echo "-T [TYPE] :"
     echo "  (*)jpg   Output .jpg file"
     echo "     png   Output .png file"
@@ -28,6 +29,13 @@ function usage
     echo "-o [FILENAME] :"
     echo "           output file name"
     echo "           (*)a.out"
+    echo "-m [MODE] :"
+    echo "  (*)dot   Digraph (layering)"
+    echo "     neato Based on spring-model"
+    echo "     twopi Radial layout"
+    echo "     circo Circle layout"
+    echo "     fdp   Undigraph"
+    echo "     patchwork Square tree"
     echo "-h :"
     echo "     Get useage" 
     echo ""
@@ -39,19 +47,21 @@ cflow_opt_file="*.c"
 cflow_opt=""
 tree_e=1
 tree_r=1
+graph="dot"
 xdot_type="-Tjpg"
 xdot_output="-o a.jpg"
 
-while getopts "a:d:e:r:T:o:h" opt; do
+while getopts ":a:d:e:r:T:o:m:fh" opt; do
     case $opt in
         a)
-            if [ "$OPTARG" -eq 1 ]; then
+            if [ "$OPTARG" == 1 ]; then
                 cflow_opt_A="-A"
             else
-                if [ "$OPTARG" -eq 2 ]; then
+                if [[ "$OPTARG" == 2 ]]; then
                     cflow_opt_A="-A -A"
                 else
-                    echo "usage: -a 0 or -a 1 or -a 2";
+                    echo "$0: invalid option argument $OPTARG";
+                    usage;
                     exit 0;
                 fi
             fi
@@ -60,19 +70,21 @@ while getopts "a:d:e:r:T:o:h" opt; do
             cflow_opt_d="-d $OPTARG"
         ;;
         e)
-            if [ $OPTARG -eq 0 ]; then
+            if [[ $OPTARG == 0 ]]; then
                 tree_e=0
-            else if [ $OPTARG -ne 1 ]; then
-                    echo "useage -e 0 or -e 1";
+            else if [ $OPTARG != 1 ]; then
+                    echo "$0: invalid option argument $OPTARG";
+                    usage;
                     exit 0;
                 fi
             fi
         ;;
         r)
-            if [ $OPTARG -eq 0 ]; then
+            if [ $OPTARG == 0 ]; then
                 tree_r=0
-            else if [ $OPTARG -ne 1 ]; then
-                    echo "useage -r 0 or -r 1";
+            else if [ $OPTARG != 1 ]; then
+                    echo "$0: invalid option argument $OPTARG";
+                    usage;
                     exit 0;
                 fi
             fi
@@ -86,7 +98,24 @@ while getopts "a:d:e:r:T:o:h" opt; do
         f)
             cflow_opt_file=$OPTARG
         ;;
-        h|?)
+        m)
+            if [[ $OPTARG == "dot" ]] || [[ $OPTARG == "neato" ]] || [[ $OPTARG == "twopi" ]] || [[ $OPTARG == "circo" ]] || [[ $OPTARG == "fdp" ]] || [[ $OPTARG == "sfdp" ]] || [[ $OPTARG == "patchwork" ]]; then
+                graph="$OPTARG";
+            else
+                echo "$0: unknow output mode $OPTARG";
+                usage;
+                exit 1;
+            fi
+        ;;
+        :)
+            echo "$0: miss option argument: -$OPTARG";
+            usage;
+            exit 1;
+        ;;
+        h|\?)
+            if [ $opt != "h" ]; then
+                echo "$0: invalid option: -$OPTARG";
+            fi
             usage ;
             exit 1;
         ;;
@@ -96,4 +125,6 @@ done
 cflow_opt="-b $cflow_opt_A $cflow_opt_d $cflow_opt_file"
 
 sudo cflow $cflow_opt| tree2dotx -e $tree_e -r $tree_r | awk '!a[$0]++' > out.dot
-dot $xdot_type out.dot $xdot_output
+$graph $xdot_type out.dot $xdot_output
+
+echo "build call graph success."
